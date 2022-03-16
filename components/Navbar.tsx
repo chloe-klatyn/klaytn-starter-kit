83cb17dcd3f6ce65c222f933375cfc6bda72e45d
chloet <chloe.t@krustuniverse.com>
2022-03-16 13:20:03 +0800

    added colours and fonts to tailwind config

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,9 @@
 const Navbar = () => {
-  return <div className="h-screen">navbar</div>;
+  return (
+    <div className="h-screen bg-grey w-1/3 p-2 border-r-2 border-grey shadow">
+      navbar
+    </div>
+  );
 };
 
 export default Navbar;
